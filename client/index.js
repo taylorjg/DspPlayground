d9417da43dcd41c9981c42be7a943ce9641d44d4
Jon Taylor <jont@cakesolutions.net>
2017-10-14 13:13:21 +0100

    Extract dsp code into separate module

--- client/index.js
+++ client/index.js
@@ -6,6 +6,7 @@ import inputSignal from './InputSignals/sine_1_16_128.json';
 // import inputSignal from './InputSignals/sine_4_16_64.json';
 // import inputSignal from './InputSignals/sine_16_32_32.json';
 // import inputSignal from './InputSignals/sine_16_128_128.json';
+import { dft, inverseDft } from '../dsp';
 
 $(document).ready(() => {
 
@@ -23,52 +24,6 @@ $(document).ready(() => {
     drawDiagram('svg-ImX', ImX, 'Im X[]');
     drawDiagram('svg-x2', x2, 'x2[]');
 });
-
-const TWO_TIMES_PI = Math.PI * 2;
-const EPSILON = 1e-13;
-
-const dft = x => {
-
-    const n = x.length;
-    const ReX = Array(n / 2 + 1).fill(0);
-    const ImX = Array(n / 2 + 1).fill(0);
-
-    for (let k = 0; k < n / 2 + 1; k++) {
-        for (let i = 0; i < n; i++) {
-            const r = TWO_TIMES_PI * k * i / n;
-            ReX[k] += x[i] * Math.cos(r);
-            ImX[k] += -x[i] * Math.sin(r);
-        }
-    }
-
-    return { ReX, ImX };
-};
-
-const inverseDft = (ReX, ImX) => {
-
-    const normalisedReX = normalise(ReX, false);
-    const normalisedImX = normalise(ImX, true);
-    const n = (ReX.length - 1) * 2;
-    const x = Array(n).fill(0);
-
-    for (let k = 0; k < n / 2 + 1; k++) {
-        for (let i = 0; i < n; i++) {
-            const r = TWO_TIMES_PI * k * i / n;
-            x[i] += normalisedReX[k] * Math.cos(r) + normalisedImX[k] * Math.sin(r);
-        }
-    }
-
-    return x.map(y => Math.abs(y) > EPSILON ? y : 0);
-};
-
-const normalise = (xs, isIm) => {
-    const nover2 = xs.length - 1;
-    return xs.map((x, index) => {
-        const v1 = x / nover2;
-        const v2 = v1 * (isIm ? -1 : 1);
-        return (!isIm && (index == 0 || index == nover2)) ? v2 / 2 : v2;
-    });
-};
 
 const drawDiagram = (id, values, title) => {
     const svg = document.getElementById(id);
---
+++ dsp/dft.js
@@ -0,0 +1,45 @@
+const TWO_TIMES_PI = Math.PI * 2;
+const EPSILON = 1e-13;
+
+export const dft = x => {
+
+    const n = x.length;
+    const ReX = Array(n / 2 + 1).fill(0);
+    const ImX = Array(n / 2 + 1).fill(0);
+
+    for (let k = 0; k < n / 2 + 1; k++) {
+        for (let i = 0; i < n; i++) {
+            const r = TWO_TIMES_PI * k * i / n;
+            ReX[k] += x[i] * Math.cos(r);
+            ImX[k] += -x[i] * Math.sin(r);
+        }
+    }
+
+    return { ReX, ImX };
+};
+
+export const inverseDft = (ReX, ImX) => {
+
+    const normalisedReX = normalise(ReX, false);
+    const normalisedImX = normalise(ImX, true);
+    const n = (ReX.length - 1) * 2;
+    const x = Array(n).fill(0);
+
+    for (let k = 0; k < n / 2 + 1; k++) {
+        for (let i = 0; i < n; i++) {
+            const r = TWO_TIMES_PI * k * i / n;
+            x[i] += normalisedReX[k] * Math.cos(r) + normalisedImX[k] * Math.sin(r);
+        }
+    }
+
+    return x.map(y => Math.abs(y) > EPSILON ? y : 0);
+};
+
+const normalise = (xs, isIm) => {
+    const nover2 = xs.length - 1;
+    return xs.map((x, index) => {
+        const v1 = x / nover2;
+        const v2 = v1 * (isIm ? -1 : 1);
+        return (!isIm && (index == 0 || index == nover2)) ? v2 / 2 : v2;
+    });
+};
