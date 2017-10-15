3bfde4a167aadd23967540c80ab61fb3683cf9c4
Jon Taylor <jont@cakesolutions.net>
2017-10-15 10:31:50 +0100

    Add outer grid lines to diagrams

--- client/chapter8/dft/index.js
+++ client/chapter8/dft/index.js
@@ -1,10 +1,15 @@
-import inputSignal from '../../../InputSignals/sine_2_128_128.json';
+// import inputSignal from '../../../InputSignals/sine_2_128_128.json';
+import inputSignal1 from '../../../InputSignals/sine_2_128_128.json';
+import inputSignal2 from '../../../InputSignals/sine_8_128_128.json';
 import { dft, inverseDft } from '../../../dsp';
 import { drawDiagram } from '../../../diagram';
 
 $(document).ready(() => {
 
-    const x = inputSignal.x;
+    // const x = inputSignal.x;
+    const signal1 = inputSignal1.x;
+    const signal2 = inputSignal2.x;
+    const x = signal1.map((value, index) => value + signal2[index]);
     const { ReX, ImX } = dft(x);
     const x2 = inverseDft(ReX, ImX);
 
--- diagram/diagram.js
+++ diagram/diagram.js
@@ -10,40 +10,26 @@ const createElement = (elementName, additionalAttributes) => {
 };
 
 export const drawDiagram = (id, values, title) => {
+
     const svg = document.getElementById(id);
     const caption = document.getElementById(`${id}-caption`);
     caption.innerHTML = title;
+
+    const MARGIN = 20;
     const w = svg.scrollWidth;
     const h = svg.scrollHeight;
-    const MARGIN = 10;
     const aw = w - 2 * MARGIN;
     const ah = h - 2 * MARGIN;
-    [1, 2, 3].forEach(i => {
-        const y = MARGIN + ah * i / 4;
-        const line = createElement('line', {
-            x1: MARGIN,
-            y1: y,
-            x2: w - 1 - MARGIN,
-            y2: y,
-            stroke: 'grey',
-            'stroke-width': 1,
-            'stroke-dasharray': [1, 4]
-        });
-        svg.appendChild(line);
-    });
-    [1, 2, 3, 4, 5, 6, 7].forEach(i => {
-        const x = MARGIN + aw * i / 8;
-        const line = createElement('line', {
-            x1: x,
-            y1: MARGIN,
-            x2: x,
-            y2: h - 1 - MARGIN,
-            stroke: 'grey',
-            'stroke-width': 1,
-            'stroke-dasharray': [1, 4]
-        });
-        svg.appendChild(line);
-    });
+
+    drawInnerHorizontalGridLines(w, h, aw, ah, MARGIN, svg);
+    drawInnerVerticalGridLines(w, h, aw, ah, MARGIN, svg);
+    drawOuterHorizontalGridLines(w, h, aw, ah, MARGIN, svg);
+    drawOuterVerticalGridLines(w, h, aw, ah, MARGIN, svg);
+    drawValues(w, h, aw, ah, MARGIN, svg, values);
+};
+
+const drawValues = (w, h, aw, ah, margin, svg,values) => {
+
     const SQUARE_SIZE = aw / 128;
     const MIN_VALUE = Math.min(...values);
     const MAX_VALUE = Math.max(...values);
@@ -52,8 +38,9 @@ export const drawDiagram = (id, values, title) => {
     const MID_VALUE = 0;
     const MID_Y = h / 2;
     const STEP = ah / RANGE;
+    
     values.forEach((value, index) => {
-        const x = MARGIN + aw / values.length * index - (SQUARE_SIZE / 2);
+        const x = margin + aw / values.length * index - (SQUARE_SIZE / 2);
         const dy = (value - MID_VALUE) * STEP;
         const y = MID_Y - dy - (SQUARE_SIZE / 2);
         const rect = createElement('rect', {
@@ -64,5 +51,79 @@ export const drawDiagram = (id, values, title) => {
             fill: 'black'
         });
         svg.appendChild(rect);
+    });
+};
+
+const drawInnerHorizontalGridLines = (w, h, aw, ah, margin, svg) => {
+    [1, 2, 3].forEach(i => {
+        const y = margin + ah * i / 4;
+        const line = createElement('line', {
+            x1: margin,
+            y1: y,
+            x2: w - 1 - margin,
+            y2: y,
+            stroke: 'grey',
+            'stroke-width': 1,
+            'stroke-dasharray': [4, 4]
+        });
+        svg.appendChild(line);
+    });
+};
+
+const drawInnerVerticalGridLines = (w, h, aw, ah, margin, svg) => {
+    [1, 2, 3, 4, 5, 6, 7].forEach(i => {
+        const x = margin + aw * i / 8;
+        const line = createElement('line', {
+            x1: x,
+            y1: margin,
+            x2: x,
+            y2: h - 1 - margin,
+            stroke: 'grey',
+            'stroke-width': 1,
+            'stroke-dasharray': [4, 4]
+        });
+        svg.appendChild(line);
+    });
+};
+
+const drawOuterHorizontalGridLines = (w, h, aw, ah, margin, svg) => {
+
+    const rect = createElement('rect', {
+        x: margin,
+        y: margin,
+        width: aw,
+        height: ah,
+        stroke: 'grey',
+        'stroke-width': 1,
+        'fill-opacity': 0
+    });
+    svg.appendChild(rect);
+
+    [0, 1, 2, 3, 4].forEach(i => {
+        const y = margin + ah * i / 4;
+        const line = createElement('line', {
+            x1: margin / 2,
+            y1: y,
+            x2: margin,
+            y2: y,
+            stroke: 'grey',
+            'stroke-width': 1
+        });
+        svg.appendChild(line);
+    });
+};
+
+const drawOuterVerticalGridLines = (w, h, aw, ah, margin, svg) => {
+    [0, 1, 2, 3, 4, 5, 6, 7, 8].forEach(i => {
+        const x = margin + aw * i / 8;
+        const line = createElement('line', {
+            x1: x,
+            y1: h - 1 - margin,
+            x2: x,
+            y2: h - 1 - (margin / 2),
+            stroke: 'grey',
+            'stroke-width': 1
+        });
+        svg.appendChild(line);
     });
 };
