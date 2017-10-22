54874cc7b30f3f7a4d94d6b6b414be72a0eb8bb5
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-10-22 11:58:00 +0100

    First step towards react spa
    Added react and babel packages
    Modified webpack config to build a single thin
    Created components hierarchy

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,37 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const glob = require('glob');
 const packageJson = require('./package.json');
 
 const serverPublic = path.join(__dirname, 'server', 'public');
-const indexJsFiles = glob.sync('./client/**/index.js');
 
-const indexJsFileToWebpackConfigObject = indexJsFile => {
-    const dirName = path.dirname(indexJsFile);
-    return {
-        entry: indexJsFile,
-        output: {
-            path: path.join(serverPublic, path.relative('./client', dirName)),
-            filename: 'bundle.js'
-        },
-        plugins: [
-            new CopyWebpackPlugin([
-                { context: dirName, from: '*.html' },
-                { context: './client', from: '*.css' }
-            ]),
-            new HtmlWebpackPlugin({
-                template: path.join(dirName, 'index.html'),
-                version: packageJson.version
-            })
-        ],
-        devtool: 'source-map',
-        devServer: {
-            contentBase: serverPublic
-        }
-    };
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: serverPublic,
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { context: './client', from: '*.html' },
+            { context: './client', from: '*.css' }
+        ]),
+        new HtmlWebpackPlugin({
+            template: './client/index.html',
+            version: packageJson.version
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: serverPublic
+    }
 };
-
-module.exports = indexJsFiles.map(indexJsFileToWebpackConfigObject);
