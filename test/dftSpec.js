6623f8034c0308a6a51abd485320c6988069125c
Jon Taylor <jont@cakesolutions.net>
2017-10-30 22:32:46 +0000

    Add unit test re DFT / inverse DFT

---
+++ test/dftSpec.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import { dft, inverseDft } from '../dsp';
+import sineWave2Hz from '../InputSignals/sine_2_128_128.json';
+
+describe('DFT tests', () => {
+
+    const TOLERANCE = 1e-12;
+
+    it('x => DFT => inverse DFT => x', () => {
+        const x1 = sineWave2Hz.x;
+        const { ReX, ImX } = dft(x1);
+        const x2 = inverseDft(ReX, ImX);
+        const n = x1.length;
+        for (let k = 0; k < n; k++) {
+            expect(x1[k]).to.be.closeTo(x2[k], TOLERANCE);
+        }
+    });
+});
